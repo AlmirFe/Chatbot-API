d78f6ea0af70914490f28c2643743390d8588ea8
Almir Ferreira <almirferreira.sj@gmail.com>
2021-11-03 18:16:54 -0300

    Adicionando metodo Post

--- chatbot/src/main/java/com/unicap/engenhariadesoftware/chatbot/controllers/ControllerOpcao.java
+++ chatbot/src/main/java/com/unicap/engenhariadesoftware/chatbot/controllers/ControllerOpcao.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,9 @@ public class ControllerOpcao {
 	@GetMapping("/opcao/{id}")
 	public Opcao mostraOpcao(@PathVariable(value="id") long id){
 		return opcaoRepositorio.findById(id);
+	}
+	@PostMapping("/opcao")
+	public Opcao adicionaOpcao(@RequestBody Opcao opcao){
+		return opcaoRepositorio.save(opcao);
 	}
 }
