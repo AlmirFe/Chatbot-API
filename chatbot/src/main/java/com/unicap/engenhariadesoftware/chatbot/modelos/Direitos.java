5cb7a2111d4402a005fdc2427756ee2f31c127aa
Almir Ferreira <almirferreira.sj@gmail.com>
2021-11-20 17:47:18 -0300

    Adicionando tabela para armazenamento de perguntas relacionadas aos direitos do estagiario

---
+++ chatbot/src/main/java/com/unicap/engenhariadesoftware/chatbot/modelos/Direitos.java
@@ -0,0 +1,44 @@
+package com.unicap.engenhariadesoftware.chatbot.modelos;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "DIREITOS_TB")
+public class Direitos implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String pergunta;
+	private String resposta;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getPergunta() {
+		return pergunta;
+	}
+	public void setPergunta(String pergunta) {
+		this.pergunta = pergunta;
+	}
+	public String getResposta() {
+		return resposta;
+	}
+	public void setResposta(String resposta) {
+		this.resposta = resposta;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	} 
+
+}
---
+++ chatbot/src/main/java/com/unicap/engenhariadesoftware/chatbot/repositorios/DireitosRepositorio.java
@@ -0,0 +1,8 @@
+package com.unicap.engenhariadesoftware.chatbot.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unicap.engenhariadesoftware.chatbot.modelos.Direitos;
+public interface DireitosRepositorio extends JpaRepository<Direitos, Long>{
+
+}
