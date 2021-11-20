b07963a0d3ccbf2691aaf96f4e9d687bad07474d
Almir Ferreira <almirferreira.sj@gmail.com>
2021-11-20 17:25:07 -0300

    Adicionando tabela para armazenamento de perguntas relacionadas a jornada de estagio

---
+++ chatbot/src/main/java/com/unicap/engenhariadesoftware/chatbot/repositorios/JornadaRepositorio.java
@@ -0,0 +1,9 @@
+package com.unicap.engenhariadesoftware.chatbot.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unicap.engenhariadesoftware.chatbot.modelos.Jornada;
+
+public interface JornadaRepositorio extends JpaRepository<Jornada, Long>{
+
+}
