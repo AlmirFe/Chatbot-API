42ada1160bbd4b05dba0c3224ec99165cdc4071b
Almir Ferreira <almirferreira.sj@gmail.com>
2021-11-20 17:53:56 -0300

    Adicionando tabela para armazenamento de perguntas relacionadas ao termo de compromisso

---
+++ chatbot/src/main/java/com/unicap/engenhariadesoftware/chatbot/repositorios/TermoCompromissoRepositorio.java
@@ -0,0 +1,8 @@
+package com.unicap.engenhariadesoftware.chatbot.repositorios;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unicap.engenhariadesoftware.chatbot.modelos.TermoCompromisso;
+
+public interface TermoCompromissoRepositorio extends JpaRepository<TermoCompromisso, Long>{
+
+}
